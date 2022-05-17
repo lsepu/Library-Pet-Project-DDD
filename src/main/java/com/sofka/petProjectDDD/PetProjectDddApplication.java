d0cb191065149332b55e7fe7a36d1aae1cf3bc87
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-17 17:31:01 -0500

    Project intialized

---
+++ src/main/java/com/sofka/petProjectDDD/PetProjectDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.petProjectDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProjectDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/petProjectDDD/PetProjectDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.petProjectDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
